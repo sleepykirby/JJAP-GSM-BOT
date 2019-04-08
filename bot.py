e65d9c05d0dfaf553adcabf00b93de673f74739f
limeslime <y2s0605@gmail.com>
2019-04-08 13:25:28 +0900

    ㅇㅇ

--- bot.py
+++ bot.py
@@ -1,13 +1,10 @@
 import asyncio
 import discord
 from discord.ext import commands
-from selenium import webdriver
 from datetime import datetime
 from bs4 import BeautifulSoup
 
 client = discord.Client()
-driver=webdriver.Chrome('D:/Drivers')
-driver.implicitly_wait(3)
 
 
 # 1-6에서 생성된 토큰을 이곳에 입력해주세요.
@@ -23,7 +20,7 @@ async def on_ready():
     # 디스코드에는 현재 본인이 어떤 게임을 플레이하는지 보여주는 기능이 있습니다.
     # 이 기능을 사용하여 봇의 상태를 간단하게 출력해줄 수 있습니다.
     #await client.change_presence(game=discord.Game(name="반갑습니다 :D", type=1))
-    await client.change_presence(activity=discord.Game("게임 안"),status=discord.Status.idle,afk=True)
+    await client.change_presence(activity=discord.Game("!커맨드"),status=discord.Status.idle,afk=True)
 
 # 봇이 새로운 메시지를 수신했을때 동작되는 코드입니다.
 @client.event
@@ -40,20 +37,25 @@ async def on_message(message):
     elif message.content.startswith('!클리어'):
         await client.clear()
     elif message.content.startswith('!급식'):
-        driver.get('http://www.gsm.hs.kr/xboard/board.php?tbnum=8')
+        url=urlopen('http://www.gsm.hs.kr/xboard/board.php?tbnum=8')
+        html=url.read().decode('utf-8')
+        soup=bs(html,'html.parser')
+        cont=soup.find('li',class_="today").find_all('span',class_="content")
+        now=datetime.now()
         if(now.hour<=8):
             #아침식사
-            driver.find_element_by_class_name("today ").find_element_by_class_name("food_list_box").find_element_by_class_name("slider_food_list slider_food25 cycle-slideshow").find_element
+            await message.channel.send(now.month+'/'+now.day+' 아침 식사 : '+'\n'+cont[0].get_text())
         elif(now.hour<=12):
             #점심식사
+            await message.channel.send(now.month+'/'+now.day+' 점심 식사 : '+'\n'+cont[1].get_text())
         else:
             #저녁식사
-
+            await message.channel.send(now.month+'/'+now.day+' 저녁 식사 : '+'\n'+cont[2].get_text())
+        
     elif message.content.startswith('!김동')
         await message.channel.send('국')
         #웹크롤링 급식
-    #else: #위의 if에 해당되지 않는 경우
-        #메시지를 보낸사람을 호출하며 말한 메시지 내용을 그대로 출력해줍니다.
+    else: #위의 if에 해당되지 않는 경우
         #await message.channel.send('누물보')
 
 
