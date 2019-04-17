c62a84ac3c8772fbdca8934c5ee91bca854a168e
limeslime <y2s0605@gmail.com>
2019-04-17 16:07:47 +0900

    Update bot_newdeal.py

--- bot_newdeal.py
+++ bot_newdeal.py
@@ -10,6 +10,11 @@ bot=commands.Bot(command_prefix="!")
 
 BOT_TOKEN = "NTU4MTEzMzQ5NDczNDAyODgz.XKvVqQ.zpxrsvOYSzvoMShFSv49Ya92QU4"
 
+#변수 목록
+
+
+
+#봇 커맨드
 @bot.event
 async def on_read():
     print("Client Logged in")
@@ -21,20 +26,7 @@ async def hello(ctx):
 
 @bot.command(pass_context=True)
 async def 급식(ctx):
-    print(discord.User.display_name)
-    html = urlopen("http://www.gsm.hs.kr/xboard/board.php?tbnum=8").read().decode("utf-8")
-    soup = bs(html, "html.parser")
-    cal = soup.select("#xb_fm_list > div.calendar > ul > li.today > div > div > div > div > span.content")
-    foodlist=[]
-    for eat in cal:
-        eat = eat.text.split("\n")
-        eat_tmp = []
-        for e in eat:
-            e=e.strip().split()[0].strip("/")
-            eat_tmp.append(e)
-        if eat_tmp.count("*") > 0:
-            del eat_tmp[eat_tmp.index("*"):]
-        foodlist.append(eat_tmp)
+    
     now=datetime.now()
 
 
@@ -54,8 +46,10 @@ async def 소라고둥(ctx,*args):
     if(len(args)>=2):
         if(args[1]=='add'):
             #추가
+            a=0
         elif(args[1]=='del'):
             #삭제
+            b=0
     elif(len(args)==1):
         if(args[1]=="help"|args[1]=="?"):
             await ctx.send("")
