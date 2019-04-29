a80eaa1da3a0fdb805ad32b2db719541e59afd78
limeslime <y2s0605@gmail.com>
2019-04-29 12:52:55 +0900

    급식 DB

--- bot_newdeal.py
+++ bot_newdeal.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 import secrets
 import sqlite3
 import bot_2048
+import foodCrawler
 
 
 
@@ -30,55 +31,57 @@ async def on_ready():
 
 
 @bot.command(pass_context=True)
-async def 급식(ctx):#냠냠
-    
-    now=datetime.now()
-    tomorrow=now+timedelta(days=1)
-    날짜=str(now.day)+"("+요일[now.weekday()]+")"
-    if((now.hour*60+now.minute)<480):#아침
-        시간='아침'
-    elif((now.hour*60+now.minute)<810):#점심
-        시간='점심'
-    elif((now.hour*60+now.minute)<1110):#저녁
-        시간='저녁'
-    else:
-        tws=tomorrow.strftime('%A')
-        if(tws=='Sunday'):
-            tw=6
-        elif(tws=='Monday'):
-            tw=0
-        elif(tws=='Tuesday'):
-            tw=1
-        elif(tws=='Wednesday'):
-            tw=2
-        elif(tws=='Thursday'):
-            tw=3
-        elif(tws=='Friday'):
-            tw=4
-        elif(tws=='Saturday'):
-            tw=5
-        
-        날짜=str(int(tomorrow.strftime('%d')))+"("+요일[tw]+")"
-        시간='아침'
-    print(날짜)
-    dbn='foodtable-'
-    dbn+=str(now.year)+'-'+str(now.month)
-    dbn+='.db'
-    conn = sqlite3.connect(dbn)
-    cur = conn.cursor()
-    sql="SELECT foods FROM ft WHERE date=\'%s\' AND time=\'%s\'"%(날짜,시간)#sql문 생성
-    print(sql)
-    cur.execute(sql)
-    rlt=cur.fetchone()
-    rltlist=rlt[0].split(' ')
-    print(rltlist)
-    급식날짜=str(now.year)+"년 "+str(now.month)+"월 "+날짜+" "+시간+" 식단표"#embed 박스 제목
-    급식목록=''#embed 박스 내용 
-    for r in rltlist:
-        급식목록+='- '+r+'\n'
-    e=discord.Embed(title=급식날짜,description=급식목록,colour=12777215)
-    await ctx.send(embed=e)
-    conn.close()
+async def 급식(ctx,*args):#냠냠
+    if(len(args)==0):
+        now=datetime.now()
+        tomorrow=now+timedelta(days=1)
+        날짜=str(now.day)+"("+요일[now.weekday()]+")"
+        if((now.hour*60+now.minute)<480):#아침
+            시간='아침'
+        elif((now.hour*60+now.minute)<810):#점심
+            시간='점심'
+        elif((now.hour*60+now.minute)<1110):#저녁
+            시간='저녁'
+        else:
+            tws=tomorrow.strftime('%A')
+            if(tws=='Sunday'):
+                tw=6
+            elif(tws=='Monday'):
+                tw=0
+            elif(tws=='Tuesday'):
+                tw=1
+            elif(tws=='Wednesday'):
+                tw=2
+            elif(tws=='Thursday'):
+                tw=3
+            elif(tws=='Friday'):
+                tw=4
+            elif(tws=='Saturday'):
+                tw=5
+            
+            날짜=str(int(tomorrow.strftime('%d')))+"("+요일[tw]+")"
+            시간='아침'
+        print(날짜)
+        dbn='foodtable-'
+        dbn+=str(now.year)+'-'+str(now.month)
+        dbn+='.db'
+        conn = sqlite3.connect(dbn)
+        cur = conn.cursor()
+        sql="SELECT foods FROM ft WHERE date=\'%s\' AND time=\'%s\'"%(날짜,시간)#sql문 생성
+        print(sql)
+        cur.execute(sql)
+        rlt=cur.fetchone()
+        rltlist=rlt[0].split(' ')
+        print(rltlist)
+        급식날짜=str(now.year)+"년 "+str(now.month)+"월 "+날짜+" "+시간+" 식단표"#embed 박스 제목
+        급식목록=''#embed 박스 내용 
+        for r in rltlist:
+            급식목록+='- '+r+'\n'
+        e=discord.Embed(title=급식날짜,description=급식목록,colour=12777215)
+        await ctx.send(embed=e)
+        conn.close()
+    elif(args[0]=='DB'):
+        foodCrawler.run()
 
 @bot.command(pass_context=True)
 async def test(ctx,*args):#args는 !명령어 args[0] args[1] args[2] ... 순
@@ -139,16 +142,40 @@ async def 깃헙(ctx):
 #     await ctx.send('https://twitter.com/k3id_moung')
 
 @bot.command(pass_context=True)
-async def help(ctx):#명령어 목록 출력
-    msg=''
-    msg+='- !급식\n'
-    msg+='- !소라고둥\n'
-    msg+='- !김동\n'
-    msg+='- !샌즈\n'
-    msg+='- !_2048\n'
-    msg+='- !깃헙\n'
-    #msg+='- !짹짹\n'
-    e=discord.Embed(title='JJAP GSM BOT 명령어 목록',description=msg,colour=12777215)
-    await ctx.send(embed=e)
+async def help(ctx,*args):#명령어 목록 출력
+    if(len(args)==0):
+        msg=''
+        msg+='- !급식\n'
+        msg+='- !소라고둥\n'
+        msg+='- !김동\n'
+        msg+='- !샌즈\n'
+        msg+='- !_2048\n'
+        msg+='- !깃헙\n'
+        #msg+='- !짹짹\n'
+        e=discord.Embed(title='JJAP GSM BOT 명령어 목록',description=msg,colour=12777215)
+        await ctx.send(embed=e)
+    elif(len(args)==1):
+        if(args[0]=='급식'):
+            제목='!급식'
+            내용='!급식 DB : 매달 한 번씩 한 달 분량 급식 다운로드'
+        elif(args[0]=='김동'):
+            제목='!김동'
+            내용='국'
+        elif(args[0]=='샌즈'):
+            제목='!샌즈'
+            내용='와 샌즈! 파피루스! 언더테일!'
+        elif(args[0]=='소라고둥'):
+            제목='!소라고둥'
+            내용='질문-답변 서비스, 현재 개발중'
+        elif(args[0]=='깃헙'):
+            제목='!깃헙'
+            내용='짭 GSM 봇 개발 Github 주소 제공'
+        elif(args[0]=='_2048'):
+            제목='!_2048'
+            내용='재밌고 신나는 2048 게임을 디코 봇으로!, 현재 개발중'
+        e=discord.Embed(title=제목,description=내용)
+        await ctx.send(embed=e)
+
+
 
 bot.run(BOT_TOKEN)
--- foodCrawler.py
+++ foodCrawler.py
@@ -3,104 +3,107 @@ from urllib.request import urlopen
 import urllib.parse
 from datetime import datetime
 
+def run():
 # # 우리학교 급식 사이트 받아오기
-RSS = "http://www.gsm.hs.kr/xboard/board.php"
-values = {"tbnum" : "8"}
-query = urllib.parse.urlencode(values)
-url = RSS+ "?"+query
-html = urlopen(url).read().decode("utf-8")
-soup = bs(html, "html.parser")
+    RSS = "http://www.gsm.hs.kr/xboard/board.php"
+    values = {"tbnum" : "8"}
+    query = urllib.parse.urlencode(values)
+    url = RSS+ "?"+query
+    html = urlopen(url).read().decode("utf-8")
+    soup = bs(html, "html.parser")
 
-# # 오늘의 급식표 받아오기
-cal=soup.find("div", class_="calendar").find("li", class_="today").find_all("span", class_="content")
-foodlist = []
-for eat in cal:
-    eat = eat.text.split("\n")
-    eat_tmp = []
-    for e in eat:
-        e=e.strip().split()[0].strip("/")
-        eat_tmp.append(e)
-    if eat_tmp.count("*") > 0:
-        del eat_tmp[eat_tmp.index("*"):]
-    foodlist.append(eat_tmp)
+    # # 오늘의 급식표 받아오기
+    cal=soup.find("div", class_="calendar").find("li", class_="today").find_all("span", class_="content")
+    foodlist = []
+    for eat in cal:
+        eat = eat.text.split("\n")
+        eat_tmp = []
+        for e in eat:
+            e=e.strip().split()[0].strip("/")
+            eat_tmp.append(e)
+        if eat_tmp.count("*") > 0:
+            del eat_tmp[eat_tmp.index("*"):]
+        foodlist.append(eat_tmp)
 
-# # 오늘의 급식표 출력
-foodtime = ['아침', '점심', '저녁']
-for foodt, foodli in zip(foodtime, foodlist):
-    print(foodt)
-    for food in foodli:
-        print(food)
-    print()
+    # # 오늘의 급식표 출력
+    foodtime = ['아침', '점심', '저녁']
+    for foodt, foodli in zip(foodtime, foodlist):
+        print(foodt)
+        for food in foodli:
+            print(food)
+        print()
 
-# # 이번 달 급식표 받아오기
-cal=soup.find("div", class_="calendar").find_all("span", class_="content")
-foodlist = []
-for eat in cal:
-    eat = eat.text.split("\n")
-    eat_tmp = []
-    for e in eat:
-        e=e.strip().split()[0].strip("/")
-        eat_tmp.append(e)
-    if eat_tmp.count("*") > 0:
-        del eat_tmp[eat_tmp.index("*"):]
-    foodlist.append(eat_tmp)
 
-# # 이번 달 급식표 날짜와 음식 둘 다 가져오기
-table=soup.select("#xb_fm_list > div.calendar > ul > li > div")
-tablelist=[]
-for t in table:
-    t_tmp = []
-    for t_item in t:
-        if t_item != "\n":
-            t_split=t_item.text.strip().split("\n\n\n\n\n\n")
-            if len(t_split)==1 and len(t_split[0])>0:
-                t_tmp.append(t_split[0])
-            elif len(t_split)>1:
-                t_tmp.append(t_split)
+
+    # # 이번 달 급식표 받아오기
+    cal=soup.find("div", class_="calendar").find_all("span", class_="content")
+    foodlist = []
+    for eat in cal:
+        eat = eat.text.split("\n")
+        eat_tmp = []
+        for e in eat:
+            e=e.strip().split()[0].strip("/")
+            eat_tmp.append(e)
+        if eat_tmp.count("*") > 0:
+            del eat_tmp[eat_tmp.index("*"):]
+        foodlist.append(eat_tmp)
+
+    # # 이번 달 급식표 날짜와 음식 둘 다 가져오기
+    table=soup.select("#xb_fm_list > div.calendar > ul > li > div")
+    tablelist=[]
+    for t in table:
+        t_tmp = []
+        for t_item in t:
+            if t_item != "\n":
+                t_split=t_item.text.strip().split("\n\n\n\n\n\n")
+                if len(t_split)==1 and len(t_split[0])>0:
+                    t_tmp.append(t_split[0])
+                elif len(t_split)>1:
+                    t_tmp.append(t_split)
+                else:
+                    continue
+        tablelist.append(t_tmp)
+
+    for day in range(len(tablelist)):
+        if len(tablelist[day])<=1:
+            continue
+        for eats in range(len(tablelist[day][1])):
+            tablelist[day].append(tablelist[day][1][eats].split("\n"))
+        del tablelist[day][1]
+        for eats in range(1,len(tablelist[day])):
+            for foods in range(len(tablelist[day][eats])):
+                tablelist[day][eats][foods]=tablelist[day][eats][foods].strip().split()
+                if len(tablelist[day][eats][foods])>0:
+                    tablelist[day][eats][foods]=tablelist[day][eats][foods][0].strip("/")
+            if tablelist[day][eats].count("*") > 0:
+                del tablelist[day][eats][tablelist[day][eats].index("*"):]
+
+    # # DB 저장
+    now=datetime.now()
+    # dbn='foodtable-'
+    # dbn+=str(now.year)+'-'+str(now.month)
+    # dbn+='.db'
+
+    import sqlite3
+    conn = sqlite3.connect('./foodtable'+'-'+str(now.year)+'-'+str(now.month)+'.db')
+    cur = conn.cursor()
+
+    cur.execute('DROP TABLE IF EXISTS ft')
+    cur.execute('CREATE TABLE ft (date text, time text, foods text)')
+
+    for day in range(len(tablelist)):
+        time = ['아침', '점심','저녁']
+        for i in range(len(time)):
+            if i>=len(tablelist[day])-1 or len(tablelist[day][i+1]) <= 0:
+                cur.execute('INSERT INTO ft values(?,?,?)', (tablelist[day][0], time[i], "없음"))
             else:
-                continue
-    tablelist.append(t_tmp)
+                cur.execute('INSERT INTO ft values(?,?,?)', (tablelist[day][0], time[i], " ".join(tablelist[day][i+1])))
 
-for day in range(len(tablelist)):
-    if len(tablelist[day])<=1:
-        continue
-    for eats in range(len(tablelist[day][1])):
-        tablelist[day].append(tablelist[day][1][eats].split("\n"))
-    del tablelist[day][1]
-    for eats in range(1,len(tablelist[day])):
-        for foods in range(len(tablelist[day][eats])):
-            tablelist[day][eats][foods]=tablelist[day][eats][foods].strip().split()
-            if len(tablelist[day][eats][foods])>0:
-                tablelist[day][eats][foods]=tablelist[day][eats][foods][0].strip("/")
-        if tablelist[day][eats].count("*") > 0:
-            del tablelist[day][eats][tablelist[day][eats].index("*"):]
+    cur.execute('SELECT * FROM ft')
+    rlt = cur.fetchall()
 
-# # DB 저장
-now=datetime.now()
-# dbn='foodtable-'
-# dbn+=str(now.year)+'-'+str(now.month)
-# dbn+='.db'
-
-import sqlite3
-conn = sqlite3.connect('./foodtable'+'-'+str(now.year)+'-'+str(now.month)+'.db')
-cur = conn.cursor()
-
-cur.execute('DROP TABLE IF EXISTS ft')
-cur.execute('CREATE TABLE ft (date text, time text, foods text)')
-
-for day in range(len(tablelist)):
-    time = ['아침', '점심','저녁']
-    for i in range(len(time)):
-        if i>=len(tablelist[day])-1 or len(tablelist[day][i+1]) <= 0:
-            cur.execute('INSERT INTO ft values(?,?,?)', (tablelist[day][0], time[i], "없음"))
-        else:
-            cur.execute('INSERT INTO ft values(?,?,?)', (tablelist[day][0], time[i], " ".join(tablelist[day][i+1])))
-
-cur.execute('SELECT * FROM ft')
-rlt = cur.fetchall()
-
-for r in rlt:
-    print("날짜 : %s, 시간: %s, 메뉴: %s"% r)
-    
-conn.commit()
-conn.close()
+    for r in rlt:
+        print("날짜 : %s, 시간: %s, 메뉴: %s"% r)
+        
+    conn.commit()
+    conn.close()
